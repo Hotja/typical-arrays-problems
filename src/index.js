a7ed0c28cbb5b3e3169262fde64a774b7c37816e
unknown <dudechka@mail.ru>
2020-03-11 20:35:27 +0300

    init:minmaxavg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  mymin = array[0];
+  for (i = 1; i < array.length; ++i) {
+     if (array[i] < mymin) mymin = array[i];
+  }
+  return mymin;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  mymax = array[0];
+  for (i = 1; i < array.length; ++i) {
+      if (array[i] > mymax) mymax = array[i];   
+  }
+  return mymax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined) return 0;
+  if (array.length === 0) return 0;
+  let l = 0;
+  let sum = 0;
+  let mid = 0;
+  for (i = 0; i < array.length; i++) {
+      l = array.length;
+      sum += array[i];
+  }
+  mid = sum / l;
+  return mid;
 }
